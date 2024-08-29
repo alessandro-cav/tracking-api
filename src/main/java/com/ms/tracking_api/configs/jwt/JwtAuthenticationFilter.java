90681675f1ba70b60527628a7f6b88fd1e0c9ea3
alessandro <alessandro-cav@hotmail.com>
2024-08-29 11:07:55 -0300

    tirando espaço e arrumando a indentação

--- src/main/java/com/ms/tracking_api/configs/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/ms/tracking_api/configs/jwt/JwtAuthenticationFilter.java
@@ -41,8 +41,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         userEmail = jwtService.extractUsername(jwt);
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-            if (jwtService.isTokenValid(jwt, userDetails)){
-                UsernamePasswordAuthenticationToken authToken =  new UsernamePasswordAuthenticationToken(
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
                         null,
                         userDetails.getAuthorities()
