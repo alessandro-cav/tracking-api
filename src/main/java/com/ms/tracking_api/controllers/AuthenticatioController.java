cb4e0e3b9a29026c0bd6e400c7c165e9b0a1b729
alessandro <alessandro-cav@hotmail.com>
2024-11-01 15:52:14 -0300

    mundando a url

--- src/main/java/com/ms/tracking_api/controllers/AuthenticatioController.java
+++ src/main/java/com/ms/tracking_api/controllers/AuthenticatioController.java
@@ -8,15 +8,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 @Tag(name = "Authentication Controller", description = "APIs relacionadas à autenticação")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthenticatioController {
 
     private final AuthenticationService service;
