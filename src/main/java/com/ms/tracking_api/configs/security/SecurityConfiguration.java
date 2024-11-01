b579e7753c71dc250262e0f75158bedd9394f6e2
alessandro <alessandro-cav@hotmail.com>
2024-11-01 16:37:10 -0300

    outra tentativa

--- src/main/java/com/ms/tracking_api/configs/security/SecurityConfiguration.java
+++ src/main/java/com/ms/tracking_api/configs/security/SecurityConfiguration.java
@@ -2,6 +2,11 @@ package com.ms.tracking_api.configs.security;
 
 
 import com.ms.tracking_api.configs.jwt.JwtAuthenticationFilter;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +24,20 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@OpenAPIDefinition(
+        servers = {
+                // Caso o swagger nao abrir na nuvem , coloque a / no campo da url
+                @Server(url = "http://localhost:8080/ms-tracking", description = "Default Server URL")
+        }
+)
+@SecurityScheme(
+        name = "Autenticacao",
+        description = "Autenticação via JWT",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
 public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
@@ -27,6 +46,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+
                 .cors(cors -> cors
                         .configurationSource(request -> {
                             CorsConfiguration config = new CorsConfiguration();
@@ -36,17 +56,17 @@ public class SecurityConfiguration {
                             config.setAllowCredentials(true);
                             return config;
                         }))
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.disable()) // Desativa o CSRF. Remover essa linha se múltiplas sessões forem usadas
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
-                        .requestMatchers("/api/v1/auth/**", "/api/v1/recuperar/**").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll() // Permite acesso à documentação da API
+                        .requestMatchers("/api/v1/auth/**", "/api/v1/recuperar/**").permitAll() // Permite acesso às rotas de autenticação e recuperação
+                        .anyRequest().authenticated() // Exige autenticação para todas as outras requisições
                 )
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Define a política de criação de sessão como sem estado (stateless)
                 )
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .authenticationProvider(authenticationProvider) // Configura o provedor de autenticação
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); // Adiciona o filtro JWT antes do filtro de autenticação padrão
 
         return http.build();
     }
