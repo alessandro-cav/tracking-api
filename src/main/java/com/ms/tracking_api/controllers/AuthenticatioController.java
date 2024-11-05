813ed362adfae9077f05e7b36be809868bb2bd4c
alessandro <alessandro-cav@hotmail.com>
2024-11-05 09:45:30 -0300

    porta aleatoria

--- src/main/java/com/ms/tracking_api/controllers/AuthenticatioController.java
+++ src/main/java/com/ms/tracking_api/controllers/AuthenticatioController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 @Tag(name = "Authentication Controller", description = "APIs relacionadas à autenticação")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthenticatioController {
 
     private final AuthenticationService service;
