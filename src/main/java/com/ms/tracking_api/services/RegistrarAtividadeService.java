d9b540910f50ca423479aa1306750f0137c286bf
alessandro <alessandro-cav@hotmail.com>
2024-12-12 15:54:35 -0300

    Refatorando regitro de atividade

--- src/main/java/com/ms/tracking_api/services/RegistrarAtividadeService.java
+++ src/main/java/com/ms/tracking_api/services/RegistrarAtividadeService.java
@@ -5,6 +5,7 @@ import com.ms.tracking_api.dtos.requests.QRCodeRequest;
 import com.ms.tracking_api.dtos.responses.ReciboResponse;
 import com.ms.tracking_api.dtos.responses.RegistrarAtividaderResponse;
 import com.ms.tracking_api.entities.*;
+import com.ms.tracking_api.enuns.StatusCandidatura;
 import com.ms.tracking_api.enuns.TipoAcesso;
 import com.ms.tracking_api.handlers.BadRequestException;
 import com.ms.tracking_api.repositories.RegistrarAtividadeRepository;
@@ -29,43 +30,93 @@ public class RegistrarAtividadeService {
 
     private final UsuarioVagaService usuarioVagaService;
 
-
-    @Transactional(readOnly = true)
+    @Transactional
     public RegistrarAtividaderResponse registrarAtividade(QRCodeRequest request) {
         Vaga vaga = this.vagaService.buscarVagaPeloId(request.getIdVaga());
         Usuario usuario = this.usuarioService.buscarUsuarioPeloId(request.getIdUsuario());
         Evento evento = this.eventoService.buscarEventoPeloId(request.getIdEvento());
 
-        UsuarioVaga usuarioVaga = this.usuarioVagaService.findByUsuarioIdUsuarioAndVagaIdVaga(usuario.getIdUsuario(), vaga.getIdVaga());
+        UsuarioVaga usuarioVaga = buscarUsuarioVaga(usuario, vaga);
+        validarUsuarioVaga(usuarioVaga, evento);
 
+        RegistrarAtividade atividade = registrarEntradaOUSaida(usuario, vaga);
+
+        atividade = this.repository.save(atividade);
+        return gerarRespostaAtividade(atividade);
+    }
+
+
+    private UsuarioVaga buscarUsuarioVaga(Usuario usuario, Vaga vaga) {
+        return this.usuarioVagaService.findByUsuarioIdUsuarioAndVagaIdVaga(usuario.getIdUsuario(), vaga.getIdVaga());
+    }
+
+    private void validarUsuarioVaga(UsuarioVaga usuarioVaga, Evento evento) {
         if (!usuarioVaga.getVaga().getEvento().getIdEvento().equals(evento.getIdEvento())) {
             throw new BadRequestException("A vaga que o usuário está vinculado não pertence a esse evento");
         }
 
-        RegistrarAtividade atividade =  buscarUltimaAtividadePorUsuario(usuario.getIdUsuario(), vaga.getIdVaga());
-
-        if( atividade  == null ){
-            atividade = new RegistrarAtividade();
-            atividade.setTipoAcesso(TipoAcesso.ENTRADA);
-        }else {
-            validarIntervaloDeTempo(atividade.getDataHora());
-            atividade.setTipoAcesso(TipoAcesso.SAIDA);
+        if (usuarioVaga.getStatusCandidatura() == StatusCandidatura.PENDENTE || usuarioVaga.getStatusCandidatura() == StatusCandidatura.RECUSADA) {
+            throw new BadRequestException("A candidatura de trabalho está pendente ou foi recusada para este evento, portanto, não é possível realizar a entrada.");
         }
-        atividade.setUsuario(usuario);
-        atividade.setVaga(vaga);
-
-        atividade = this.repository.save(atividade);
-
-        RegistrarAtividaderResponse atividaderResponse = new RegistrarAtividaderResponse();
-        atividaderResponse.setIdRegistrarAtividade(atividade.getIdRegistrarAtividade());
-        atividaderResponse.setNome(atividade.getUsuario().getNome());
-        atividaderResponse.setVaga(atividade.getVaga().getVaga());
-        atividaderResponse.setTipoAcesso(atividade.getTipoAcesso());
-        atividaderResponse.setEvento(atividade.getVaga().getEvento().getNome());
-        return atividaderResponse;
     }
 
-    /*private void validarIntervaloDeTempo(LocalDateTime ultimaAtividade) {
+    private RegistrarAtividade registrarEntradaOUSaida(Usuario usuario, Vaga vaga) {
+        RegistrarAtividade atividade = buscarUltimaAtividadePorUsuario(usuario.getIdUsuario(), vaga.getIdVaga());
+
+        if (atividade == null) {
+            return registrarEntrada(usuario, vaga);
+        } else {
+            validarIntervaloDeTempo(atividade.getDataHora());
+            return registrarSaida(usuario, vaga);
+        }
+    }
+
+    private RegistrarAtividade registrarEntrada(Usuario usuario, Vaga vaga) {
+        RegistrarAtividade atividade = new RegistrarAtividade();
+        atividade.setTipoAcesso(TipoAcesso.ENTRADA);
+        atividade.setUsuario(usuario);
+        atividade.setVaga(vaga);
+        return atividade;
+    }
+
+    private RegistrarAtividade registrarSaida(Usuario usuario, Vaga vaga) {
+        RegistrarAtividade atividade = new RegistrarAtividade();
+        atividade.setTipoAcesso(TipoAcesso.SAIDA);
+        atividade.setUsuario(usuario);
+        atividade.setVaga(vaga);
+        return atividade;
+    }
+
+    private RegistrarAtividaderResponse gerarRespostaAtividade(RegistrarAtividade atividade) {
+        RegistrarAtividaderResponse response = new RegistrarAtividaderResponse();
+        response.setIdRegistrarAtividade(atividade.getIdRegistrarAtividade());
+        response.setNome(atividade.getUsuario().getNome());
+        response.setVaga(atividade.getVaga().getVaga());
+        response.setTipoAcesso(atividade.getTipoAcesso());
+        response.setEvento(atividade.getVaga().getEvento().getNome());
+        return response;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //-----------------
+    private void validarIntervaloDeTempo(LocalDateTime ultimaAtividade) {
         if (ultimaAtividade != null) {
             long horasDeDiferenca = Duration.between(ultimaAtividade, LocalDateTime.now()).toHoursPart();
 
@@ -73,19 +124,7 @@ public class RegistrarAtividadeService {
                 throw new BadRequestException("O registro de saída deve ser gerado pelo menos 2 horas após o inicio do evento.");
             }
         }
-    }*/
-
-    private void validarIntervaloDeTempo(LocalDateTime ultimaAtividade) {
-        if (ultimaAtividade != null) {
-            long minutosDeDiferenca = Duration.between(ultimaAtividade, LocalDateTime.now()).toMinutes();
-
-            // Verifica se a diferença é menor que 5 minutos
-            if (minutosDeDiferenca < 5) {
-                throw new BadRequestException("O registro de saída deve ser gerado pelo menos 5 minutos após o inicio do evento.");
-            }
-        }
     }
-
     @Transactional(readOnly = true)
     private RegistrarAtividade buscarUltimaAtividadePorUsuario(Long idUsuario, Long idVaga) {
         return this.repository.
@@ -101,6 +140,7 @@ public class RegistrarAtividadeService {
         cr.setNome(usuario.getNome());
         cr.setRg(usuario.getRg());
         cr.setValor(vaga.getValor());
+        cr.setCpf(usuario.getCpf());
         return cr;
     }
 
