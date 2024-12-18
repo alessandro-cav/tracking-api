4ed25187e39adf9fff6904fca8c2cc54977bf32f
alessandro <alessandro-cav@hotmail.com>
2024-12-18 17:38:40 -0300

    Criação do convite com pre cadastro

---
+++ src/main/java/com/ms/tracking_api/dtos/responses/ConviteResponse.java
@@ -0,0 +1,24 @@
+package com.ms.tracking_api.dtos.responses;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConviteResponse implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+    private String nome;
+
+    private String email;
+
+}
