b92de5b4439a55b7d5ddaf0dc4824ae4a5ce24d5
alessandro <alessandro-cav@hotmail.com>
2025-01-02 23:02:40 -0300

    Criando a Candidatura

---
+++ src/main/java/com/ms/tracking_api/dtos/responses/UsuarioCandidatoResponse.java
@@ -0,0 +1,33 @@
+package com.ms.tracking_api.dtos.responses;
+
+import com.ms.tracking_api.enuns.Genero;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioCandidatoResponse implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long idUsuario;
+
+    private String nome;
+
+    private String cpf;
+
+    private String rg;
+
+    private String telefone;
+
+    private String email;
+
+    private String imagem;
+}
