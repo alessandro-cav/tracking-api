d24fbf1ad21bfd3e4994164784589268645c00f9
alessandro <alessandro-cav@hotmail.com>
2025-01-31 17:11:57 -0300

    Adicionando id do usuario ao token

--- src/main/java/com/ms/tracking_api/configs/jwt/JwtService.java
+++ src/main/java/com/ms/tracking_api/configs/jwt/JwtService.java
@@ -1,6 +1,6 @@
 package com.ms.tracking_api.configs.jwt;
 
-import com.ms.tracking_api.entities.Usuario;
+import com.ms.tracking_api.entities.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -37,9 +37,10 @@ public class JwtService {
             Map<String, Object> extraClaims,
             UserDetails userDetails
     ) {
-        if (userDetails instanceof Usuario usuario) {
-            extraClaims.put("id", usuario.getIdUsuario()); // Adicionando o id ao token
+        if (userDetails instanceof User user) {
+            extraClaims.put("id", user.getId()); // Adicionando o ID ao token
         }
+
         return Jwts
                 .builder()
                 .setClaims(extraClaims)
@@ -49,6 +50,7 @@ public class JwtService {
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
+
 
 
     public boolean isTokenValid(String token, UserDetails userDetails) {
