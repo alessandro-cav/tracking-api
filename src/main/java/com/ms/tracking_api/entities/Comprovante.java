39ad9a8b7b3736262a6e66b23c0fd28c0c6573a0
alessandro <alessandro-cav@hotmail.com>
2025-02-04 10:12:03 -0300

    arruamndo o nome

--- src/main/java/com/ms/tracking_api/entities/Comprovante.java
+++ src/main/java/com/ms/tracking_api/entities/Comprovante.java
@@ -17,7 +17,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "tb_vaga")
+@Table(name = "tb_comprovante")
 public class Comprovante implements Serializable {
 
     private static final long serialVersionUID = 1L;
