e3a181f10f39d3ff04db5e885baf1c173538cf8c
alessandro <alessandro-cav@hotmail.com>
2025-02-06 08:57:36 -0300

    iniciando a multi-tenacy do usuario

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_user (email, nome, role, senha,status_usuario)
-SELECT 'root@gmail.com', 'root', 'ADMIN', '$2a$12$W4ga0wBDtSMXGY0y41/PAuY8o0E.SGucCpddVRY6gvZhPxA16bpGi', 'ATIVO'
+INSERT INTO tb_user (email, nome, role, senha,status_usuario,cnpj_banco)
+SELECT 'root@gmail.com', 'root', 'ADMIN', '$2a$12$W4ga0wBDtSMXGY0y41/PAuY8o0E.SGucCpddVRY6gvZhPxA16bpGi', 'ATIVO','20555110000127'
 WHERE NOT EXISTS (SELECT email FROM tb_user WHERE id = 1);
